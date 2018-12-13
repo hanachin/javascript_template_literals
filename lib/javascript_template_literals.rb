fb15adac11f89cd74a08f8ff5c48bddb87036134
Seiei Miyagi <hanachin@gmail.com>
2018-12-13 22:31:24 +0900

    Add tagged template feature

--- lib/javascript_template_literals.rb
+++ lib/javascript_template_literals.rb
@@ -8,10 +8,26 @@ module JavascriptTemplateLiterals
     str.gsub(/\$\{([^\}]*)\}/) { b.eval($1) }
   end
 
+  def self.template_arguments(b, str)
+    strings = []
+    exps = []
+    loop do
+      h, _, str = str.partition(/\$\{([^\}]*)\}/)
+      strings << h
+      break unless $1
+      exps << b.eval($1)
+    end
+    [strings, *exps]
+  end
+
   refine(Object) do
     def `(str)
       b = binding.of_caller(1)
-      JavascriptTemplateLiterals.interpolate(b, str)
+      JavascriptTemplateLiterals.interpolate(b, str).tap do |ret|
+        ret.define_singleton_method(:to_ary) do
+          JavascriptTemplateLiterals.template_arguments(b, str)
+        end
+      end
     end
   end
 end
--- spec/javascript_template_literals_spec.rb
+++ spec/javascript_template_literals_spec.rb
@@ -11,4 +11,23 @@ not ${2 * a + b}.
     not 20.
     EOS
   end
+
+  def my_tag((strings, person_exp, age_exp))
+    str0 = strings[0]
+    str1 = strings[1]
+
+    if age_exp > 99
+      age_str = "centenarian"
+    else
+      age_str = "youngster"
+    end
+
+    `${str0}${person_exp}${str1}${age_str}`
+  end
+
+  it "pass the template literals to a tag method" do
+    person = "Mike"
+    age = 28
+    expect(my_tag`That ${ person } is a ${ age }`).to eq("That Mike is a youngster")
+  end
 end
